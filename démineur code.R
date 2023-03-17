8f6ae0ca19207761da9cc99c35bff1d77174f0f1
EmmaSinibaldi <114212255+EmmaSinibaldi@users.noreply.github.com>
2023-03-17 14:09:34 +0100

    Update démineur code.R

--- démineur code.R
+++ démineur code.R
@@ -1,11 +1,13 @@
 
-
+#Données de départ
 
 nb_mines <- 40
 nb_col <- 20
 nb_lig <- 24
 
 
+
+#Création d'un plateau de jeu vide (rempli de O)
 plateau <- matrix(0, nb_lig, nb_col)
 plateau
 
@@ -14,10 +16,32 @@ positions_mines <- cbind(sample(1:nb_lig, size = nb_mines, replace = TRUE),
 
 
 
+
+
 #position des bombes sur le plateau de jeu
 
 for (i in 1:nb_mines) {
-  plateau[positions_mines[i, 1], positions_mines[i, 2]] <-"X"
+  plateau[positions_mines[i, 1], positions_mines[i, 2]] <-"-1"
 }
 plateau
 
+
+
+
+
+#Calcul du nombre de mines adjacentes aux bombes
+
+
+for (i in 1:nb_lig) {
+  for (j in 1:nb_col) {
+    if (plateau[i,j] == -1) {
+      for (k in max(1, i-1):min(nb_lig, i+1)) {
+        for (l in max(1, j-1):min(nb_col, j+1)) {
+          if (plateau[k,l] != -1) {
+            plateau[k,l] <- plateau[k,l] + 1
+          }
+        }
+      }
+    }
+  }
+}
