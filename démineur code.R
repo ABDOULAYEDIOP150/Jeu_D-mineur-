db12f3510929bdac16ef7dcf06e3276997f1eba3
EmmaSinibaldi <114212255+EmmaSinibaldi@users.noreply.github.com>
2023-03-17 13:24:09 +0100

    Update démineur code.R

--- démineur code.R
+++ démineur code.R
@@ -9,12 +9,15 @@ nb_lig <- 24
 plateau <- matrix(0, nb_lig, nb_col)
 plateau
 
-mines <- function(plateau, nb_mines) {
-  for (i in 1:n_mines) {
-    repeat {
-      plateau <- sample(1:nb_lig, 1)
-      plateau <- sample(1:nb_col, 1)
-    }
-  }
-  return(plateau)
-  }
+positions_mines <- cbind(sample(1:nb_lig, size = nb_mines, replace = TRUE),
+                   sample(1:nb_col, size = nb_mines, replace = TRUE))
+
+
+
+#position des bombes sur le plateau de jeu
+
+for (i in 1:nb_mines) {
+  plateau[positions_mines[i, 1], positions_mines[i, 2]] <-"X"
+}
+plateau
+
